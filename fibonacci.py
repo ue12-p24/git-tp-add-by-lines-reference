6f04eff4494a5b0a1ff0a4c04c7af6b704ef7dd5
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2021-09-25 12:36:11 +0200

    v1 : cached fibo

--- fibonacci.py
+++ fibonacci.py
@@ -2,11 +2,17 @@
 la suite de fibonacci
 """
 
-def fibo(n):
+# on optimise
+
+from functools import cache
+
+@cache
+def fibo_cached(n):
     """
-    # une implémentation naïve et inefficace
+    en cachant les résultats on arrive
+    à une complexité linéaire
     """
     if n <= 1:
         return n
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fibo_cached(n-1) + fibo_cached(n-2)
